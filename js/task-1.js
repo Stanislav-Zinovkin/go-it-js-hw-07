2560cff25227afcc8279ed45d25cc92c92c1dce1
Stanislav-Zinovkin <stas14zinovkin@gmail.com>
2025-02-15 11:56:09 +0200

    Hw#7-js-v0.1

--- js/task-1.js
+++ js/task-1.js
@@ -1,10 +1,8 @@
-function slugify(title) {
-  const lowerCasetitle = title.toLowerCase();
-  const slug = lowerCasetitle.split(" ").join("-");
-  return slug;
-}
+const categories = document.querySelectorAll("ul#categories > .item");
+console.log(`Number of categories ${categories.length}`);
 
-console.log(slugify("Arrays for beginners"));
-console.log(slugify("English for developer"));
-console.log(slugify("Ten secrets of JavaScript"));
-console.log(slugify("How to become a JUNIOR developer in TWO WEEKS"));
+categories.forEach((category) => {
+  const title = category.querySelector("h2").textContent;
+  const itemCount = category.querySelectorAll("ul li").length;
+  console.log(`Category ${title}, Numder of categories ${itemCount} `);
+});
--- js/task-3.js
+++ js/task-3.js
@@ -1,15 +1,10 @@
-function filterArray(numbers, value) {
-  const newArray = [];
-  for (let i = 0; i < numbers.length; i += 1) {
-    if (numbers[i] > value) {
-      newArray.push(numbers[i]);
-    }
+const input = document.getElementById("name-input");
+const output = document.getElementById("name-output");
+input.addEventListener("input", () => {
+  const name = input.value.trim();
+  if (name) {
+    output.textContent = name;
+  } else {
+    output.textContent = "Anonymous";
   }
-  return newArray;
-}
-
-console.log(filterArray([1, 2, 3, 4, 5], 3));
-console.log(filterArray([1, 2, 3, 4, 5], 4));
-console.log(filterArray([1, 2, 3, 4, 5], 5));
-console.log(filterArray([12, 24, 8, 41, 76], 38));
-console.log(filterArray([12, 24, 8, 41, 76], 20));
+});

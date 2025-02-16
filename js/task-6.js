48636fcc2dc2cc5c38956561809d9ae90fab8b09
Stanislav-Zinovkin <stas14zinovkin@gmail.com>
2025-02-16 16:54:59 +0200

    Hw07-js-v0.2

--- js/task-6.js
+++ js/task-6.js
@@ -3,3 +3,41 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+function createBoxes(amount) {
+  const boxesContainer = document.getElementById("boxes");
+  boxesContainer.innerHTML = "";
+
+  const boxes = [];
+  let size = 30;
+  for (let i = 0; i < amount; i++) {
+    const box = document.createElement("div");
+    box.style.width = `${size}px`;
+    box.style.height = `${size}px`;
+    box.style.backgroundColor = getRandomHexColor();
+    box.style.marginBottom = "10px";
+
+    boxes.push(box);
+    size += 10;
+  }
+  boxesContainer.append(...boxes);
+}
+
+const createButton = document.querySelector("[data-create]");
+const destroyButton = document.querySelector("[data-destroy]");
+const input = document.querySelector('input[type="number"]');
+
+createButton.addEventListener("click", () => {
+  const amount = Number(input.value);
+
+  if (amount >= 1 && amount <= 100) {
+    createBoxes(amount);
+    input.value = "";
+  } else {
+    alert("Please enter a number between 1 and 100.");
+  }
+});
+
+destroyButton.addEventListener("click", () => {
+  destroyBoxes();
+});
